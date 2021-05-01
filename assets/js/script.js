0a84e653b8673b4e893ea192559f9b2b192d041f
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-30 22:00:13 -0600

    added methods to dynamically display current temp, wind, humidity and saved city button in DOM with minimal bootstrap styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,14 @@ $("#searchBtn").on("click", function () {
   console.log(cityInput);
   //clear input field
   $("#citySearch").val("");
+
+  //display the saved city search
+  //**need to connect to local storage for saving and retrieval**
+  var savedContainer = $("#savedCities");
+  var savedCity = $("<button>");
+  savedCity.text(cityInput).addClass("btn btn-secondary col-12 mt-3");
+  savedContainer.append(savedCity);
+
   //create a variable that combines api url with city + key
   var weatherKey = "5255530bf1f1204d64609824d51b20e5";
   var requestUrl = [
@@ -30,23 +38,37 @@ $("#searchBtn").on("click", function () {
 
     //parse the object to glean relevant data
     var cityName = response.name;
-    var temp = response.main.temp;
-    var wind = response.wind.speed;
-    var humidity = response.main.humidity;
-
-    //select containers to generate data elements
-    var saved = $("#savedCities");
-    var current = $("#currentDisplay");
-    var forecast = $("#forecastDisplay");
-
-    //display the saved city search
+    var tempData = response.main.temp;
+    var windData = response.wind.speed;
+    var humData = response.main.humidity;
 
     //display current weather data with dynamically generated elements
+
+    //display city name
+    var current = $("#currentDisplay");
     var cityShow = $("<h1>");
-    cityShow.text(cityName);
+    cityShow.text(cityName).addClass("h1");
     current.append(cityShow);
 
+    //display temp
+    var tempShow = $("<h2>");
+    tempShow.text("Temp: " + tempData + " °F").addClass("h3");
+    current.append(tempShow);
+
+    //display wind
+    var windShow = $("<h2>");
+    windShow.text("Wind: " + windData + " MPH").addClass("h3");
+    current.append(windShow);
+
+    //display humidity
+    var humShow = $("<h2>");
+    humShow.text("Humidity: " + humData + "%").addClass("h3");
+    current.append(humShow);
+
+    //display UV index (color)
+
     //display the 5 day forecast
+    var forecast = $("#forecastDisplay");
   });
 });
 
