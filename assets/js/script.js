ea8490a5d884f2f9141ef9c7297972ffaf3a8864
Joshua Abeyta <mentaltemple@gmail.com>
2021-04-30 21:04:11 -0600

    created vars to parse and store returned data for current weather from open weather api, selected html elements using jquery where data will be displayed, created jquery method to append city name to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,16 @@
 
 //listen for click from search button
 $("#searchBtn").on("click", function () {
-  var city = $("#citySearch").val();
-  console.log(city);
+  var cityInput = $("#citySearch").val();
+  console.log(cityInput);
   //clear input field
-
+  $("#citySearch").val("");
   //create a variable that combines api url with city + key
   var weatherKey = "5255530bf1f1204d64609824d51b20e5";
   var requestUrl = [
     "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=" +
+      cityInput +
+      "&units=imperial&appid=" +
       weatherKey,
   ];
   console.log(requestUrl);
@@ -26,8 +26,27 @@ $("#searchBtn").on("click", function () {
     url: requestUrl,
     method: "GET",
   }).then(function (response) {
-    console.log("Ajax Reponse \n-------------");
     console.log(response);
+
+    //parse the object to glean relevant data
+    var cityName = response.name;
+    var temp = response.main.temp;
+    var wind = response.wind.speed;
+    var humidity = response.main.humidity;
+
+    //select containers to generate data elements
+    var saved = $("#savedCities");
+    var current = $("#currentDisplay");
+    var forecast = $("#forecastDisplay");
+
+    //display the saved city search
+
+    //display current weather data with dynamically generated elements
+    var cityShow = $("<h1>");
+    cityShow.text(cityName);
+    current.append(cityShow);
+
+    //display the 5 day forecast
   });
 });
 
